c38ad9ac0cb9e1a4493fc78c668b7472209a56bd
Noble Mathew <noby.matty@gmail.com>
2020-09-07 13:21:23 +1000

    Removed main storyboard

--- AboutCanada/AppDelegate.swift
+++ AboutCanada/AppDelegate.swift
@@ -5,6 +5,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let aboutViewController = AboutViewController()
+        window?.rootViewController = aboutViewController
+        window?.makeKeyAndVisible()
         return true
     }
 }
